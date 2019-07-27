15648268aa1777850db806cb332c70c5e7dec982
Andrei <andreifbaicu@gmail.com>
2019-07-27 14:24:50 +0300

    Added new Package

---
+++ design-patterns/src/main/java/com/sda/patterns/behavioral/observer/listeners/EmailNotificationListener.java
@@ -0,0 +1,16 @@
+package com.sda.patterns.behavioral.observer.listeners;
+
+import java.io.File;
+
+public class EmailNotificationListener implements EventListener {
+    private String email;
+
+    public EmailNotificationListener(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public void update(String eventType, File file) {
+        System.out.println("Email to "+email+" : Someone performed "+eventType);
+    }
+}
---
+++ design-patterns/src/main/java/com/sda/patterns/behavioral/observer/listeners/EventListener.java
@@ -0,0 +1,7 @@
+package com.sda.patterns.behavioral.observer.listeners;
+
+import java.io.File;
+
+public interface EventListener {
+    void update(String eventType, File file);
+}
